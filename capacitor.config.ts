168f06bd9ea57f1de27200fbf9cccf5560720aff
johannanguyen <johannatnguyen@gmail.com>
2026-02-14 20:24:29 -0700

    add ios platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tar0bunny.bobarush',
+  appName: 'Boba Rush',
+  webDir: 'build'
+};
+
+export default config;
